98de8a25ac72f04f736498891334b156f3226e89
roccminton <luis.aniello@larocca.de>
2021-11-23 15:47:17 +0100

    Working example (without mutation)

--- Evolve.jl
+++ Evolve.jl
@@ -1,8 +1,8 @@
-include("Multitype.jl")
+include("Infinitetypes.jl")
 include("MainFunctions.jl")
 
 import .Gillespie
-import .Multitype
+import .InfiniteTypes
 
 using LinearAlgebra
 
@@ -10,23 +10,26 @@ using LinearAlgebra
 #Setup Model parameters
 ntypes = 10
 
-b = fill(1.0,ntypes)
-d = fill(0.9,ntypes)
-c = Diagonal(fill(10^(-5),ntypes))
+b(x) = 1.0
+d(x) = 0.9
+c(x,y) = 10^(-5)
+
 mutation = [
         0.0 0.5
         0.0 0.5
         ]
 μ = 0.0
 t = 0:200
-n_0 = fill(10,ntypes)
+n_0 = Dict(1=>[100.0,b(1),d(1)])
 
 model_parameter = (
         birth = b,
         death = d,
         competition = c,
+        compdict = InfiniteTypes.generatecompdict(n_0,c),
         M = mutation ,
-        μ = μ
+        μ = μ,
+        diff = 1
         )
 
 #execute the simulation
@@ -35,10 +38,12 @@ history = Gillespie.run_gillespie(
         t,
         n_0,
         model_parameter,
-        Multitype.execute!,
-        Multitype.rates!
+        InfiniteTypes.execute!,
+        InfiniteTypes.rates!,
+        InfiniteTypes.setuprates(b,d,n_0),
+        InfiniteTypes.setuphistory(t,n_0)
         )
 
 using Plots
 
-plot(t,[history[:,i] for i in 1:length(n_0)])
+#plot(t,[history[:,i] for i in 1:length(n_0)])
