ecdfbcd10d35b721bd2e695f7e2af4a85f4e2c77
roccminton <luis.aniello@larocca.de>
2022-05-04 11:45:14 +0200

    easy way to execute simulations

--- NonRandomMating/ExecuteExample.jl
+++ NonRandomMating/ExecuteExample.jl
@@ -1,104 +1,34 @@
-include("../DiploidModel2.jl")
+include("Toolkit.jl")
 include("../Plotting.jl")
 
-import .DiploidModel2
+import .ToolkitNRM
 import .PlotFromDicts
-using BenchmarkTools
-using Statistics
-using CSV
+
 using Plots
-using DataFrames
 
-replace_NaN(v) = map(x -> isnan(x) ? zero(x) : x, v)
-
-function execute_once(dni,N,K,tend)
-
-        b = 1.0
-        d = 0.9
-
-        parameter = (
-                birth = b,
-                death = d,
-                competition = (b-d)/K,
-                μ = dni,
-                Nloci = N,
-                K = K,
-                rates = "allbirthrates!"
-        )
-
-        t = 0:tend
-        x0 = DiploidModel2.generatehealthypopulation(K)
-
-        #execute the simulation
-        return DiploidModel2.rungillespie(t,x0,parameter)
-end
-
-function execute_and_mean(dni,N,K,tend,n)
-        hs = [execute_once(dni,N,K,tend) for _ in 1:n]
-        return Dict(
-                k => [
-                        mean(hs[i][k][t] for i ∈ 1:n)
-                for t ∈ 1:tend+1]
-        for k in keys(hs[1]))
-end
-
-function averageoverlast(history,key,last)
-        data = history[key]
-        return mean(view(data,last:length(data)))
-end
-
-function get_equilibrium(Ns,dnis,Ks,tend)
-        #create data frame
-        df = DataFrame(
-                Nloci = Int[],
-                dni = Float64[],
-                K = Int[],
-                PopSize=Float64[],
-                Prev=Float64[],
-                MutationLoad=Float64[]
-        )
-        #set timespan to average over for equilibrium
-        last = ceil(Int,tend/2)
-        #loop over all instances
-        for N ∈ Ns, dni ∈ dnis, K ∈ Ks
-                println("Currently at N=$N,dni=$dni,K=$K")
-                row = Float64[N,dni,K]
-                #number of runs depend on average size
-                #n = ceil(Int,100_000/K)
-                n=5
-                history=execute_and_mean(dni,N,K,tend,n)
-                popsize = averageoverlast(history,"PopSize",last)
-                push!(row,popsize)
-                push!(row,averageoverlast(history,"Ill",last)/popsize)
-                push!(row,averageoverlast(history,"ML",last)/popsize)
-                push!(df, row)
-        end
-        return df
-end
-
-get_equilibrium(Ns::Number,dnis,Ks,tend) = get_equilibrium([Ns,],dnis,Ks,tend)
-get_equilibrium(Ns::Number,dnis::Number,Ks,tend) = get_equilibrium([Ns,],[dnis,],Ks,tend)
-get_equilibrium(Ns::Number,dnis,Ks::Number,tend) = get_equilibrium([Ns,],dnis,[Ks,],tend)
-get_equilibrium(Ns,dnis::Number,Ks,tend) = get_equilibrium(Ns,[dnis,],Ks,tend)
-get_equilibrium(Ns,dnis::Number,Ks::Number,tend) = get_equilibrium(Ns,[dnis,],[Ks,],tend)
-get_equilibrium(Ns,dnis,Ks::Number,tend) = get_equilibrium(Ns,dnis,[Ks,],tend)
-
-#----------------
-
-Ns = vcat(1:10,15:5:50,60:10:100,150:50:500,600:100:1000)
-dnis = vcat(1:20,25:5:50,60:10:100,150:50:500,600:100:1000)
-Ks = vcat(100:100:1_000,1_500:500:10_000,11_000:1_000:50_000,55_000:5_000:100_000)
-
+#(constant) Population size
 K = 10_000
-dni = 1.0
-
-history = get_equilibrium(Ns,dnis,10_000,100)
-
+#Number of loci/genes
+N = 60
+#Number of Generations
+tend = 5000
+#de novo mutation rate
+dni = 0.8
+#number of runs to average over
+n = 50
+#path to save data at
 abs_path = "/home/larocca/github/Gillespie_Algorithm/NonRandomMating/Output/ConstPopSize/"
-#filename = "K=$(Ks[1])-$(Ks[end]),dni=$(dnis[1])-$(dnis[end]),N=$(Ns[1])-$(Ns[end])"
-filename = "K=$(K),dni=$(dnis[1])-$(dnis[end]),N=$(Ns[1])-$(Ns[end])"
-#safe DataFrame
-CSV.write(
-        abs_path * "Data/" * filename,
-        history
-)
+
+#run simulations
+#wf_history = ToolkitNRM.execute_and_mean(K,dni,N,tend,n,ToolkitNRM.execute_wf)
+#cont_history = ToolkitNRM.execute_and_mean(K,dni,N,tend,n,ToolkitNRM.execute_cont)
+wf_history = ToolkitNRM.execute_wf(K,dni,N,tend)
+cont_history = ToolkitNRM.execute_cont(K,dni,N,tend)
+
+
+#save data
+ToolkitNRM.save_data(abs_path,"Cont_K=$K,dni=$dni,Nloci=$N",cont_history,tend)
+ToolkitNRM.save_data(abs_path,"WF_K=$K,dni=$dni,Nloci=$N",wf_history,tend)
+
+#plot data
+plot(ToolkitNRM.plot_MLP(wf_history),ToolkitNRM.plot_MLP(cont_history),layout=(2,1))
